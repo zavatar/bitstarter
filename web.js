d7369ea0e62d0dd71341ac09b30c46d4f3ce9576
zhumengAWS <zhumeng1989@gmail.com>
2013-07-16 12:49:04 +0000

    display icon meng

--- web.js
+++ web.js
@@ -1,14 +1,11 @@
 var fs = require('fs');
-
 var express = require('express');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html").toString());
-});
-
 var port = process.env.PORT || 8080;
+
+var app = express.createServer(express.logger());
+app.use(express.static(__dirname));
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
